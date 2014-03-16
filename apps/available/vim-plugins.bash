96b6ca355ccc5041178c536c1245fcb12c5676ef
Paul Edenburg <paul@i2s.nl>
2014-03-16 18:09:14 +0100

    vim configuratie een stuk verbeterd. Airline is gebundeld, .vim en .vimrc zijn nu symlinks

--- apps/available/vim-plugins.bash
+++ apps/available/vim-plugins.bash
@@ -1,39 +1,28 @@
-#!/bin/sh -x
+#!/bin/bash 
+#
+# This file is executed everytime the user logs in (and this app is enabled)
 
-# copy a directory if it doesn't exist on the target
-function install-plugin(){
-	if [ ! -d ~/.vim/bundle/$1 ]
-	then
-		cp -r ${BASH_IT}/files/vim/$1 ~/.vim/bundle/$1
-	fi	
-}
-
-
-# paste the .vimrc file
-if [ ! -f ~/.vimrc ]; then
-	cp .vimrc ~/.vimrc
-else
-	cp ~/.vimrc ~/.vimrc.backup.$( date +'%Y%m%d_%H%M' )
-	cp .vimrc ~/.vimrc
+# backup the .vim directory if it already exists
+if [ -d ~/.vim ]
+then
+	mv ~/.vim ~/.vim.backup
 fi
 
-# create the autoload directory
-if [ ! -d ~/.vim/autoload ]; then
-	mkdir -p ~/.vim/autoload
+# create a symlink to the vim directory if it doesn't
+# exist yet
+if [ ! -h ~/.vim ]
+then
+	#cp -r ${BASH_IT}/files/vim/$1 ~/.vim/bundle/$1
+	ln -s ${BASH_IT}/files/vim ~/.vim
+fi	
+
+# backup an existing .vimrc file
+if [  -e ~/.vimrc ]; then
+	mv ~/.vimrc ~/.vimrc.backup
 fi
 
-# create the bundle directory
-if [ ! -d ~/.vim/bundle ];then
-	mkdir -p ~/.vim/bundle
+# create a symlink to the .vimrc file when needed
+if [ ! -h ~/.vimrc ]; then
+	#cp .vimrc ~/.vimrc
+	ln -s ${BASH_IT}/files/.vimrc ~/.vimrc
 fi
-
-# puth the pathogen script in the autoload dir
-if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
-	cp pathogen.vim ~/.vim/autoload
-fi
-
-# install the various plugins
-install-plugin vim-surround
-install-plugin nerdtree
-install-plugin tcomment_vim
-install-plugin emmet-vim
