df49faf0bb050e73d535f9e53fde841ad739af62
vagrant <vagrant@socsoc>
2014-06-26 13:39:18 +0000

    foutje opgelost

--- aliases/available/tmux.aliases.bash
+++ aliases/available/tmux.aliases.bash
@@ -3,4 +3,4 @@ cite about-alias
 about-alias 'general aliases'
 
 # kill all active tmux sessions
-alias tkas="tmux ls | awk '{print $1}' | while read session; do tmux kill-session -t ${session%*:}; done"
+alias tkas='tmux ls | cut -d " " -f 1 | while read session; do tmux kill-session -t ${session%*:}; done'
