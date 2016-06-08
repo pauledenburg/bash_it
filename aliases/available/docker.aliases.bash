3540343ccbc45aa38c471c85372383a608d0631c
Paul Edenburg <paul@i2s.nl>
2016-06-08 22:05:26 +0200

    aliases toegevoegd voor docker

---
+++ aliases/available/docker.aliases.bash
@@ -0,0 +1,9 @@
+cite 'about-alias'
+about-alias 'common docker abbreviations'
+
+# Aliases
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias di='docker images'
+alias drmi='docker rmi'
+alias drmia='docker rmi $(docker images -q -f dangling=true)'
