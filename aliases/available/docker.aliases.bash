9e2bbffa23c33f435faae086264106d42f6dc50b
Paul Edenburg <paul@i2s.nl>
2016-09-02 17:23:15 +0200

    aliases for docker added

--- aliases/available/docker.aliases.bash
+++ aliases/available/docker.aliases.bash
@@ -5,7 +5,13 @@ about-alias 'common docker abbreviations'
 alias dps='docker ps'
 alias dpsa='docker ps -a'
 alias di='docker images'
+alias db='docker build'
+alias de='docker exec -it'
 alias drm='docker rm'
 alias drmf='docker rm -f'
 alias drmi='docker rmi'
 alias drmia='docker rmi $(docker images -q -f dangling=true)'
+
+# docker-compose
+alias dc='docker-compose'
+alias dcps='docker-compose ps'
