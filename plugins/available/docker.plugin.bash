d54625b1930c170f7129e7ddd87c7e3a11d789cc
Paul Edenburg <paul@medido.com>
2018-08-12 23:09:24 +0200

    added function 'docker-bash-root' which gives a bash-shell for a docker container

--- plugins/available/docker.plugin.bash
+++ plugins/available/docker.plugin.bash
@@ -1,7 +1,7 @@
 cite about-plugin
 about-plugin 'docker helper functions'
 
-function docker-bash {
+function dbash {
   about 'goes into the bash shell of the specified running container'
   group 'docker'
 
@@ -9,4 +9,12 @@ function docker-bash {
   docker exec -it $1 bash
 }
 
+
+function docker-bash-root {
+  about 'goes into the bash shell of the specified running container as root user'
+  group 'docker'
+
+  echo "Executing bash on container $1"
+  docker exec -it -u root $1 bash
+}
 
