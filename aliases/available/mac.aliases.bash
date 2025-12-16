b99ec547f8ee899d6d1a325bae6e00fbc882f902
Paul Edenburg <paul@mm2.local>
2025-12-16 18:01:48 +0100

    added "reboot" alias for mm2

--- aliases/available/mac.aliases.bash
+++ aliases/available/mac.aliases.bash
@@ -3,3 +3,5 @@ about-alias 'mac aliases'
 
 # List directory contents
 alias updatedb='sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist'
+
+alias reboot='sudo fdesetup authrestart'
